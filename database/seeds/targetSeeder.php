d41cec976c0832822f3fb0c6cfb680545b12ddb6
te-te <te-te@naver.com>
2017-06-14 16:54:37 +0900

    fix error / modify target seed

--- database/seeds/targetSeeder.php
+++ database/seeds/targetSeeder.php
@@ -13,7 +13,7 @@ class targetSeeder extends Seeder
     {
         DB::table('target')->insert([
             'num'=>null,
-            'name'=>'감나무',
+            'name'=>'이현필',
             'profile_image'=>'/images/profileImage/1.jpg',
             'age'=>'70',
             'gender'=>'남',
@@ -21,29 +21,48 @@ class targetSeeder extends Seeder
             'cellphone'=>'0109999999',
             'zip_code'=>'153132',
             'main_address' =>'대구시 북고 대현동',
-            'rest_address' => '응애빌딩',
+            'rest_address' => '영진빌딩',
             'latitude'=>'35',
             'longitude'=>'35',
             'disability_main'=>'없음',
             'disability_sub'=>'없음',
-            'comment'=>'응애',
+            'comment'=>'아픕니다',
         ]);
         DB::table('target')->insert([
             'num'=>null,
-            'name'=>'응애빌런',
+            'name'=>'곽대효',
             'profile_image'=>'/images/profileImage/1.jpg',
             'age'=>'50',
             'gender'=>'여',
             'telephone'=>'0535544555',
             'cellphone'=>'0109955999',
             'zip_code'=>'153132',
-            'main_address' =>'아이언맨',
-            'rest_address' => '응애빌딩',
+            'main_address' =>'북구시 대구 현필동',
+            'rest_address' => '현필시티',
             'latitude'=>'35',
             'longitude'=>'35',
             'disability_main'=>'없음',
             'disability_sub'=>'없음',
             'comment'=>'빌런임니다~~!',
         ]);
+
+        DB::table('target')->insert([
+            'num'=>null,
+            'name'=>'조찬호',
+            'profile_image'=>'/images/profileImage/1.jpg',
+            'age'=>'70',
+            'gender'=>'남',
+            'telephone'=>'0535884555',
+            'cellphone'=>'0105323499',
+            'zip_code'=>'135132',
+            'main_address' =>'네모시 바지구 스펀지동',
+            'rest_address' => '핑핑이 아파트',
+            'latitude'=>'35',
+            'longitude'=>'32',
+            'disability_main'=>'없음',
+            'disability_sub'=>'없음',
+            'comment'=>'데헷',
+        ]);
+
     }
 }
