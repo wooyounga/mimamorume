d19e41e3bc4f99a671706cbb1c174221a7e0bbdd
kong <baretta_x3@naver.com>
2017-06-21 21:18:57 +0900

    app login merge

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -123,14 +123,13 @@ class LoginController extends Controller
        }
 
        if($check_id == true && $check_pw == true) {
-<<<<<<< HEAD
+
            /*$result = array('userid'=>$username, 'userpass'=>$password);
            echo json_encode($result);*/
            //Session::set('id', $username);
-=======
+
  //          $result = ["result" => ['id'=>$username, 'pw'=>$password]];
 //           echo json_encode($result);
->>>>>>> 63a30754f923b26d7f74ba2c87fd4ec5973b3866
            echo "success";
        }else{
            echo "failure";
