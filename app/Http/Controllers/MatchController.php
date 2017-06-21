946e864377792835e755d3d3794971e48f11fff8
kong <baretta_x3@naver.com>
2017-06-21 21:49:48 +0900

    app matching join

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -351,14 +351,20 @@ class MatchController extends Controller
 
         if($user[0]->user_type == '보호사'){
             $contract  = \DB::table('contract')
-                ->join('user','contract.sitter_id','=','user.id')
-                ->join('user','contract.family_id','=','user.id')
+                //->join('user','contract.sitter_id','=','user.id')
+                //->join('user','contract.family_id','=','user.id')
+                ->join('user', function ($join) {
+                    $join->on('user.id', '=', 'contract.sitter_id')
+                        ->orOn('user.id', '=', 'contract.family_id');
+                })
                 ->where('sitter_id',$request->get('id'))
                 ->get();
         }else{
             $contract  = \DB::table('contract')
-                ->join('user','contract.family_id','=','user.id')
-                ->join('user','contract.sitter_id','=','user.id')
+                ->join('user', function ($join) {
+                    $join->on('user.id', '=', 'contract.sitter_id')
+                        ->orOn('user.id', '=', 'contract.family_id');
+                })
                 ->where('family_id',$request->get('id'))
                 ->get();
         }
