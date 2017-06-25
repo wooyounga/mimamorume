ab2c34f9ae0f48ef1e07b6336bea32a4a17bf934
Hyeonphil Yi <artistic0326@naver.com>
2017-06-25 13:29:42 +0000

    snapshot type8

--- resources/views/monitor/snapshot.blade.php
+++ resources/views/monitor/snapshot.blade.php
@@ -67,18 +67,12 @@
                     @if($snapshot == '[]')
                         <div><h3>최근에 찍힌 스냅샷이 존재하지 않습니다</h3></div>
                     @else
-                        <div class="form-inline">
-                          <?php $i = 0; ?>
                           @foreach($snapshot as $s)
                             <span style="display:inline-block; width:300px;">
                                 <img class="thumbnail" src="{{URL::to('/')}}/images/monitor/snapShot/{{$s->upload_name}}">
                                 <br>{{$s->snapshot_type}}<br>
                               </span>
-                            <?php if($i != 3){?>
-                                  <br>
-                            <?php $i == 0; } ?>
                           @endforeach
-                        </div>
                     @endif
                    {{-- <img class="thumbnail" src="{{URL::to('/')}}/images/main_logo.png">
                     <img class="thumbnail" src="{{URL::to('/')}}/images/main/main_image_01.png">--}}
