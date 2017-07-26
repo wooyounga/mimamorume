f81dcc319201f2ba0cab365f18a0c0b68c236696
wooyounga <baretta_x3@naver.com>
2017-07-26 09:36:55 +0900

    webrtc src

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
     <video id="remoteVideo" autoplay></video>
 </div>
   {{--<script src="{{ asset('js/photo.js') }}"></script>--}}
- <script src="https://ex.app:8081/socket.io/socket.io.js"></script>
+ <script src="//{{ Request::getHost() }}:8081/socket.io/socket.io.js"></script>
  <script src="{{ asset('js/lib/adapter.js')}}"></script>
  <script src="{{ asset('js/main.js')}}"></script>
 </body>
