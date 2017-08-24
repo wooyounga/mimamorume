4b51e6d3e192cf253201fc08a39ec4a9bee6be59
te-te <te-te@naver.com>
2017-08-24 17:22:52 +0900

    match blade

--- resources/views/match/match.blade.php
+++ resources/views/match/match.blade.php
@@ -122,7 +122,7 @@
                 </ul>
             </div>
         </div>--}}
-        <table class="search table-bordered">
+        <table class="search table">
                <tr>
                     <td>구분</td>
                     <td>성별</td>
