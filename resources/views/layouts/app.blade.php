14593e408faf1d7064273215f85c301829a97fe6
wooyounga <baretta_x3@naver.com>
2017-10-08 14:29:44 +0900

    rtc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type='text/javascript' src='{{URL::to('/')}}/js/playrtc.js'></script>
     <script type='text/javascript' src='{{URL::to('/')}}/js/jquery-1.12.4.min.js'></script>
     <script type='text/javascript' src='{{URL::to('/')}}/js/jquery-migrate-1.4.1.min.js'></script>
     <!-- 부트스트랩 -->
@@ -13,7 +14,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
     <!-- 합쳐지고 최소화된 최신 자바스크립트 -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-    <script type='text/javascript' src='{{URL::to('/')}}/js/playrtc.js'></script>
+
     <!-- DatePicker -->
     <script type="text/javascript" src="{{ asset('js/jquery.simple-dtpicker.js') }}"></script>
     <link type="text/css" href="{{ asset('css/jquery.simple-dtpicker.css') }}" rel="stylesheet" />
