650f973c8b4bf1f1ebfdc64daa64b5ab147e59b9
te-te <te-te@naver.com>
2017-12-05 21:43:44 +0900

    last

--- app/Http/Controllers/GpsController.php
+++ app/Http/Controllers/GpsController.php
@@ -12,16 +12,16 @@ class GpsController extends Controller
         $this->middleware('web');
     }
 
-    public function getUserGps(Request $request) {
-        $lat = $request->input('lat');
-        $lng = $request->input('lng');
+//    public function getUserGps(Request $request) {
+//        $lat = $request->input('lat');
+//        $lng = $request->input('lng');
 	
 
 //	$targetNum = $target[0]['num'];
 //	$targetAddressLat = $target[0]['latitude'];
 //	$targetAddressLon = $target[0]['longitude'];
 	
-	$this->gpsInsert($lat, $lng);
+//	$this->gpsInsert($lat, $lng);
 
 //	$gpsDistance = $this->calculateDistance($lat, $lng, $targetAddressLat, $targetAddressLon);
 //	if($gpsDistance >= 150) {
@@ -30,35 +30,35 @@ class GpsController extends Controller
 //	    system('node ./js/fcm.js'.$message);
 //	    system('node ./js/client.js');
 //	}
-    }
+//    }
 
     //gps data insert in Database
-    private function gpsInsert($lat, $lng) {
- 	\DB::table('gps_data')->insert([
-	    'num' => null,
-	    'latitude' => $lat,
-	    'longitude' => $lng,
-	    'target_num' => 3,
-	    'created_at' => Carbon::now()->format('Y-m-d H:i:s')
-	]);
-    }
+ //   private function gpsInsert($lat, $lng) {
+// 	\DB::table('gps_data')->insert([
+//	    'num' => null,
+//	    'latitude' => $lat,
+//	    'longitude' => $lng,
+//	    'target_num' => 3,
+//	    'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+//	]);
+//   }
 
     //distance of point to point
-    private function calculateDistance($latitude, $longitude, $addressLat, $addressLon) {
-	$targetAddressLat = $addressLat;
-	$targetAddressLon = $addressLon;
-
+//    private function calculateDistance($latitude, $longitude, $addressLat, $addressLon) {
+//	$targetAddressLat = $addressLat;
+//	$targetAddressLon = $addressLon;
+//
 	//return meter
-	$R = 6378137;
-	$dLat = deg2rad($latitude - $targetAddressLat);
-	$dLon = deg2rad($longitude - $targetAddressLon);
-	$a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($targetAddressLat)) * cos(deg2rad($latitude)) *
-	    sin($dLon / 2) * sin($dLon / 2);
-	$c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+//	$R = 6378137;
+//	$dLat = deg2rad($latitude - $targetAddressLat);
+//	$dLon = deg2rad($longitude - $targetAddressLon);
+//	$a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($targetAddressLat)) * cos(deg2rad($latitude)) *
+//	    sin($dLon / 2) * sin($dLon / 2);
+//	$c = 2 * atan2(sqrt($a), sqrt(1 - $a));
 
-	$distance = $R * $c;
+//	$distance = $R * $c;
 
-	return $distance;
-    }
+//	return $distance;
+//    }
 }
 
